a0a9d7766fa329b4991a1a628346627708f91d79
rocioBanuelos <rocio.banuelos4041@alumnos.udg.mx>
2020-10-15 20:56:20 -0500

    Obtener elementos del Slice y realizar suma

--- main.go
+++ main.go
@@ -2,6 +2,24 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main() {
+	var cantidadElemento uint64
+	var numero int64
+	var resultado int64
+
+	//Se obtiene el número "n" de elementos del slice
+	fmt.Scanf("%d\n", &cantidadElemento)
+	//Creación del slice
+	s := make([]int64, 0, cantidadElemento)
+	//Leer valores del slice
+	for i := 0; i < int(cantidadElemento); i++ {
+		fmt.Scanf("%d\n", &numero)
+		s = append(s,numero)
+	}
+	//Realizar suma de elementos
+	for _, v := range s {
+		resultado += v
+	}
+	//Imprimir resultado de la suma
+	fmt.Println(resultado)	
 }
